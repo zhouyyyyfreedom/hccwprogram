ab1eb398de2fb0488ff11a39b974ca9f2651e433
zhouyu <zhouyuqq123@sina.com>
2019-08-16 13:50:14 +0800

    增加freemarker

---
+++ src/main/java/com/web/hccw/controller/BaseController.java
@@ -0,0 +1,25 @@
+package com.web.hccw.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * Created by zhouyu on 20190812
+ */
+@Controller
+@Scope("prototype")
+public class BaseController {
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public ModelAndView getImgTxtConfigList(HttpServletRequest result, HttpServletResponse response){
+        ModelAndView modelAndView = new ModelAndView("/index");
+        modelAndView.addObject("name","123");
+        return modelAndView;
+    }
+
+
+}
