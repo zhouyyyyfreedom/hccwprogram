9573082b841ccd2967681f9f0198b95cd95e7e5c
zhouyu <zhouyuqq123@sina.com>
2019-08-16 12:38:24 +0800

    创建

---
+++ src/main/java/com/web/hccw/HccwApplication.java
@@ -0,0 +1,13 @@
+package com.web.hccw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HccwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HccwApplication.class, args);
+    }
+
+}
